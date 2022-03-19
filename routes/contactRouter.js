ebcf422e8571433233b0327da2f33333bc12c926
Mullins <mullinsatheem@gmail.com>
2022-03-19 14:51:15 +0200

    added order confirmed email

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -1,77 +1,108 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-router.get('/', (req, res)=>{
-    res.send({msg:"SEND CONTACT USING POST"})
-})
+router.get("/", (req, res) => {
+  res.send({ msg: "SEND CONTACT USING POST" });
+});
 
-router.post('/',(req,res)=> {
-    const {name, email, message, subject} = req.body
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.MAIL,
-          pass: process.env.PASS
-        }
-      });
-      
-      var mailOptions = {
-        from: email,
-        to: 'mullinsatheem@gmail.com',
-        subject: `${subject}`,
-        text: `Name: ${name}
+router.post("/", (req, res) => {
+  const { name, email, message, subject } = req.body;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  var mailOptions = {
+    from: email,
+    to: "mullinsatheem@gmail.com",
+    subject: `${subject}`,
+    text: `Name: ${name}
 Email: ${email}
 Contacted You With The Below Message
     ${message}
-              `
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-          res.status(400).send({msg : "Email could not be sent" + error})
-        } else {
-          console.log('Email sent: ' + info.response);
-          res.send({msg: "Message sent succesfully"})
-        }
-      });
-})
-router.post('/DeleteUser', (req, res, next)=>{
-  const {email} = req.body
-  var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.MAIL,
-        pass: process.env.PASS
-      }
-    });
-    
-    var mailOptions = {
-      from: "MullinsWebSupp@gmail.com",
-      to: email,
-      subject: `User Deleted`,
-      text: `This is a Confirmation that your user account
-at https//www.marine-ecyclopedia.web.app has been deleted.
-`
-    };
-    
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-        res.status(400).send({msg : "Email could not be sent" + error})
-      } else {
-        console.log('Email sent: ' + info.response);
-        res.send({msg: "Message sent succesfully"})
-      }
-    });
-})
+              `,
+  };
 
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(400).send({ msg: "Email could not be sent" + error });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send({ msg: "Message sent succesfully" });
+    }
+  });
+});
+router.post("/DeleteUser", (req, res, next) => {
+  const { email } = req.body;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  var mailOptions = {
+    from: "MullinsWebSupp@gmail.com",
+    to: email,
+    subject: `User Deleted`,
+    text: `This is a Confirmation that your user account
+at https//www.marine-ecyclopedia.web.app has been deleted.
+`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(400).send({ msg: "Email could not be sent" + error });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send({ msg: "Message sent succesfully" });
+    }
+  });
+});
+router.post('/Purchase', function(req, res, next){
+  const { email } = req.body;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  var mailOptions = {
+    from: "MullinsWebSupp@gmail.com",
+    to: email,
+    subject: `ORDER CONFRIMED`,
+    text: `This is a Confirmation that your Purchase 
+at https//www.marine-ecyclopedia.web.app has been Confirmed!!!.
+`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(400).send({ msg: "Email could not be sent" + error });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send({ msg: "Message sent succesfully" });
+    }
+  });
+})
 module.exports = router;
