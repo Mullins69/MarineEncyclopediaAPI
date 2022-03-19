0738aee78eb7b7b7315cb11ff6e0d5414e85dc25
Mullins <mullinsatheem@gmail.com>
2022-03-19 16:23:19 +0200

    register email

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -105,4 +105,36 @@ at https//www.marine-ecyclopedia.web.app has been Confirmed!!!.
     }
   });
 })
+router.post('/Register', function(req, res, next){
+  const { email } = req.body;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  var mailOptions = {
+    from: "MullinsWebSupp@gmail.com",
+    to: email,
+    subject: `ACCOUNT CREATED`,
+    text: `This is a confirmation that your account has been succefully registered 
+at https//www.marine-ecyclopedia.web.app!.
+`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(400).send({ msg: "Email could not be sent" + error });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send({ msg: "Message sent succesfully" });
+    }
+  });
+})
 module.exports = router;
